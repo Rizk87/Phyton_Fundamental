b5435e3e88d4e3ae15e62b6c3e7b9cdbe28d1e0f
Rizk87 <rizk87python@gmail.com>
2022-07-05 16:50:28 +0800

    Percabangan python

---
+++ Main.py
@@ -0,0 +1,19 @@
+
+# Sequential
+print('Ibu berkata, "pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli 1 botol susu?"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
+# Percabangan
+Jumlah_botol_susu = 0
+
+if Jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol suus")
+
+print("Budi pulang kerumah")
+print("Menyampaikan hasilnya kepada Ibu")
